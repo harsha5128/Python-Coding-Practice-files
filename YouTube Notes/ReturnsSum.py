f03bc3d7705a15d6b14fe4dfd388b5d8fe9aa189
Harsha <harshavardhan5128@gmail.com>
2024-09-15 15:24:52 +0530

    Updating Validate email

--- YouTube Notes/ReturnsSum.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(s):
-    return s[::-1]
-s=input("Please enter your string value: ")
-if reverse_string(s):
-    print(s, "is a palindrome.")
-else:
-    print(s, "is not a palindrome.")
